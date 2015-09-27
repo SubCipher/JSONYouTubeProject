2170e6a83f4f4fd4b3a9dc20b8c0ec0c08771ebc
KPicart <kpicart@stepwisedesigns.com>
2015-09-26 21:45:32 -0400

    working with JSON and APIS

---
+++ JSONYouTubeProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSONYouTubeProject
+//
+//  Created by MacMan on 9/26/15.
+//  Copyright © 2015 MacManApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSONYouTubeProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSONYouTubeProject
+//
+//  Created by MacMan on 9/26/15.
+//  Copyright © 2015 MacManApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
